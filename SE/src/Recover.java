b2bcd0f63e71a85b23ff5c4ef75565fce6ec7169
aabcceccbba <kaiyuelu@gmail.com>
2018-04-23 20:55:13 -0400

    update goBack process

--- SE/src/Recover.java
+++ SE/src/Recover.java
@@ -30,6 +30,21 @@ public class Recover {
 		copy.add(next);
 	}
 	
+	public TreeNode goBack(TreeNode current, int backSteps) {
+		int currentIndex = copy.indexOf(current);
+		int newIndex = currentIndex - backSteps;
+		if(currentIndex != -1 && newIndex >= 0) {
+			current = copy.get(newIndex);
+			while(copy.size() != newIndex+1) {
+				copy.remove(copy.size()-1);
+			}
+		}
+		else {
+			System.out.println("backSteps not right or current state error!");
+		}
+		return current;
+	}
+	
 	public void process(int processID, TreeNode current){
 		  // id = 1 -> go forward to the next state
 		if(processID == 1){
@@ -44,7 +59,10 @@ public class Recover {
 		}
 		  // id = 3 -> should go back to the previous state
 		else if(processID == 3){
-		    goBack(args);
+			// accept back steps here
+			Scanner scan = new Scanner(System.in);
+			int steps = scan.nextInt();
+		    current = goBack(current, steps);
 		}
 	}
 
